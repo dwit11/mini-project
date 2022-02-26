cd9fb4afca1645f98236a5185ed339d99eb19bc7
dwit11 <dwit.java@gmail.com>
2022-02-26 19:21:18 +0900

    개발환경 구성

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export default {
+  apiKey: 'AIzaSyBd78xE5E6oxRO8xFM8xxUslDCwnJl89f0',
+  authDomain: 'toy-project-4d648.firebaseapp.com',
+  databaseURL: 'https://toy-project-4d648-default-rtdb.firebaseio.com',
+  projectId: 'toy-project-4d648',
+  storageBucket: 'toy-project-4d648.appspot.com',
+  messagingSenderId: '64424237144',
+  appId: '1:64424237144:web:8a43b2536d3d643874922d',
+  measurementId: 'G-Q3VV0HMY4Q'
+}
